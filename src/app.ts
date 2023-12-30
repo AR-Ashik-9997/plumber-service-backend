a61761a119e1a71a8b58f8cd0d28e73eca5c09bc
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2023-12-31 04:50:02 +0600

    update static folder

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 
 app.use(cors());
 app.use(cookieParser());
-
+app.use('/uploads', express.static('uploads'));
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
